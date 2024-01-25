5344427fd510a5d8d37bc0adb11fe1b1308b2ed2
JerreYjz <47430015+JerreYjz@users.noreply.github.com>
2024-01-25 11:11:20 -0500

    Update loademu.py

--- yijie/loademu.py
+++ yijie/loademu.py
@@ -4,22 +4,23 @@ import numpy as np
 from torch.utils.data import Dataset, DataLoader, TensorDataset
 
 class MLP(nn.Module):
-    def __init__(self):
+    def __init__(self, input_dim,output_dim,int_dim):
         super(MLP, self).__init__()
         self.linear =nn.Sequential(
-            nn.Linear(5, 100),
-            nn.Linear(100, 100),
+            nn.Linear(input_dim, int_dim),
+            nn.Linear(int_dim, int_dim),
             nn.Tanh(),
-            nn.Linear(100, 100),
+            nn.Linear(int_dim, int_dim),
             nn.Tanh(),
-            nn.Linear(100, 2970)
+            nn.Linear(int_dim, output_dim)
         )
     def forward(self, x):
         out = self.linear(x)
         return out
 
+model = MLP(input_dim=5,output_dim=2970,int_dim=100).to(device)
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = MLP().to(device)
 
 #load model
 PATH = "./trainedemu/trial1"
