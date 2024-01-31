eeb74bf6b7ca6d5febb23e0db63b375e23d835d3
JerreYjz <47430015+JerreYjz@users.noreply.github.com>
2024-01-31 15:43:27 -0500

    Update loademu.py

--- yijie/loademu.py
+++ yijie/loademu.py
@@ -3,22 +3,61 @@ import torch.nn as nn
 import numpy as np
 from torch.utils.data import Dataset, DataLoader, TensorDataset
 
-class MLP(nn.Module):
-    def __init__(self, input_dim,output_dim,int_dim,N_layer):
-        super(MLP, self).__init__()
-        modules=[]
-        modules.append(nn.Linear(input_dim, int_dim))
-        for n in range(N_layer):
-            modules.append(nn.Linear(int_dim, int_dim))
-            modules.append(nn.Tanh())
-        modules.append(nn.Linear(int_dim, output_dim))
-        self.linear =nn.Sequential(*modules)
+class Affine(nn.Module):
+    def __init__(self):
+        super(Affine, self).__init__()
+
+        # This function is designed for the Neuro-network to learn how to normalize the data between
+        # layers. we will initiate gains and bias both at 1 
+        self.gain = nn.Parameter(torch.ones(1))
+        self.bias = nn.Parameter(torch.zeros(1))
 
     def forward(self, x):
-        out = self.linear(x)
+        return x * self.gain + self.bias
+
+
+class MLP(nn.Module):
+    def __init__(self, input_dim, output_dim, int_dim, N_layer):
+        super(MLP, self).__init__()
+        modules=[]
+
+        # Def: we will set the internal dimension as multiple of 128 (reason: just simplicity)
+        int_dim = int_dim * 128
+
+        # Def: we will only change the dimension of the datavector using linear transformations  
+        modules.append(nn.Linear(input_dim, int_dim))
+        
+        # Def: by design, a pure block has the input and output dimension to be the same
+        for n in range(N_layer):
+            # Def: This is what we defined as a pure MLP block
+            # Why the Affine function?
+            #   R: this is for the Neuro-network to learn how to normalize the data between layers
+            modules.append(Affine())
+            modules.append(nn.Linear(int_dim, int_dim))
+            modules.append(nn.Tanh())
+        
+        # Def: the transformation from the internal dimension to the output dimension of the
+        #      data vector we intend to emulate
+        modules.append(nn.Linear(int_dim, output_dim))
+        
+        # NN.SEQUENTIAL is a PYTHORCH function DEFINED AT: https://pytorch.org/docs/stable/generated/torch.nn.Sequential.html
+        # This function stacks up layers in the modules-list in sequence to create the whole model
+        self.mlp =nn.Sequential(*modules)#
+
+    def forward(self, x):
+        #x is a cosmological parameter set you feed in the model
+        out = self.mlp(x)
         return out
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+N_thread=10
+if torch.cuda.is_available():
+    device = 'cuda'
+else:
+    device = 'cpu'
+    torch.set_num_interop_threads(N_thread) # Inter-op parallelism
+    torch.set_num_threads(N_thread) # Intra-op parallelism
+
+#device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = MLP(input_dim=5,output_dim=2970,int_dim=100,N_layer=10).to(device)
 
 
