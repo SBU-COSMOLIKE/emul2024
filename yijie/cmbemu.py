44044a193df45e14cc10afbc76856a52d7381060
JerreYjz <47430015+JerreYjz@users.noreply.github.com>
2024-02-15 18:03:03 -0500

    Update cmbemu.py

--- yijie/cmbemu.py
+++ yijie/cmbemu.py
@@ -2,12 +2,19 @@ import torch
 import torch.nn as nn
 import numpy as np
 from torch.utils.data import Dataset, DataLoader, TensorDataset
+
 N_thread=10
+
 if torch.cuda.is_available():
+
     device = 'cuda'
+
 else:
+
     device = 'cpu'
+
     torch.set_num_interop_threads(N_thread) # Inter-op parallelism
+
     torch.set_num_threads(N_thread) # Intra-op parallelism
 #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 #define model
@@ -18,15 +25,20 @@ class Affine(nn.Module):
         # This function is designed for the Neuro-network to learn how to normalize the data between
         # layers. we will initiate gains and bias both at 1 
         self.gain = nn.Parameter(torch.ones(1))
+
         self.bias = nn.Parameter(torch.zeros(1))
 
     def forward(self, x):
+
         return x * self.gain + self.bias
 
 
 class MLP(nn.Module):
+
     def __init__(self, input_dim, output_dim, int_dim, N_layer):
+
         super(MLP, self).__init__()
+
         modules=[]
 
         # Def: we will set the internal dimension as multiple of 128 (reason: just simplicity)
@@ -55,44 +67,37 @@ class MLP(nn.Module):
     def forward(self, x):
         #x is a cosmological parameter set you feed in the model
         out = self.mlp(x)
+
         return out
 
 
-#Set up the model and optimizer
-model = MLP(input_dim=5,output_dim=2970,int_dim=4,N_layer=3).to(device)
-optimizer = torch.optim.Adam(model.parameters())
-reduce_lr = True#reducing learning rate on plateau
-if reduce_lr==True:
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min',patience=10)
-
 #Set up the Covariance matrix
-fid=np.load('YZ_CMBuniform2/TT/fid.npy',allow_pickle=True)
-ell=np.arange(30,3000,1)
-ellfactor=(2*ell+1)/2
-covinv=ellfactor/fid**2
-covinv=np.diag(covinv)
+fid=np.load('YZ_samples/LHS/fid.npy',allow_pickle=True)
+
+covinv=np.load('YZ_samples/LHS/cosvarinv.npy',allow_pickle=True)
 covinv=torch.Tensor(covinv).to(device) #This is inverse of the Covariance Matrix
 
 #load in data
-samples=np.load('CMBSO/uniform2.npy',allow_pickle=True)# This is actually a latin hypercube sampling of 200k points
-TT=np.load('YZ_CMBuniform2/TT/TT_1.npy',allow_pickle=True)# Only doing TT for now
-#TE=np.load('YZ_CMBuniform2/TE/TE_1.npy',allow_pickle=True)
-#EE=np.load('YZ_CMBuniform2/EE/EE_1.npy',allow_pickle=True)
+samples=np.load('YZ_samples/LHS/coslhc_acc.npy',allow_pickle=True)# This is actually a latin hypercube sampling of 1mil points
+input_size=len(samples[0])
+data_vectors=np.load('YZ_samples/LHS/coslhc_acc_output.npy',allow_pickle=True)
 
-for i in range(2,11):
-    TTnew=np.load('YZ_CMBuniform2/TT/TT_'+str(i)+'.npy',allow_pickle=True)
-    #TEnew=np.load('YZ_CMBuniform2/TE/TE_'+str(i)+'.npy',allow_pickle=True)
-    #EEnew=np.load('YZ_CMBuniform2/EE/EE_'+str(i)+'.npy',allow_pickle=True)
-    TT=np.vstack((TT,TTnew))
-    #TE=np.vstack((TE,TEnew))
-    #EE=np.vstack((EE,EEnew))
-data_vectors=TT#np.concatenate((TT,TE,EE),axis=1)
-
+out_size=3*len(data_vectors[0])
 #assign training and validation sets
-train_samples=samples[:150000]
-train_data_vectors=data_vectors[:150000]
-validation_samples=samples[150000:]
-validation_data_vectors=data_vectors[150000:]
+vnum=0
+train_samples=[]
+train_data_vectors=[]
+validation_samples=[]
+validation_data_vectors=[]
+for ind in range(len(samples)):
+    samp=samples[ind]
+    if (0.01<samp[0]<0.035) and (0.005<samp[1]<0.85) and (30<samp[2]<90) and (0.02<samp[3]<0.75) and (0.8<samp[4]<1.2) and (1.7<samp[5]<4.5) and vnum<=int(1e5):
+        validation_samples.append(samp)
+        validation_data_vectors.append(data_vectors[ind])
+        vnum+=1
+    else:
+        train_samples.append(samp)
+        train_data_vectors.append(data_vectors[ind])
 
 train_samples=torch.Tensor(train_samples)
 train_data_vectors=torch.Tensor(train_data_vectors)
@@ -105,9 +110,11 @@ X_std  = torch.Tensor(train_samples.std(axis=0, keepdims=True))
 Y_mean=torch.Tensor(train_data_vectors.mean(axis=0, keepdims=True))
 Y_std=torch.Tensor(train_data_vectors.std(axis=0, keepdims=True))
 X_train=(train_samples-X_mean)/X_std
+X_train[:,6:]=0 # we didn't vary the last 3 parameters: mnu, w, and wa in this test, so setting them to 0 automatically after normalization
 y_train=(train_data_vectors-Y_mean)/Y_std
 
 X_validation=(validation_samples-X_mean)/X_std
+X_validation[:,6:]=0 # we didn't vary the last 3 parameters: mnu, w, and wa in this test, so setting them to 0 automatically after normalization
 y_validation=(validation_data_vectors-Y_mean)/Y_std
 
 #load the data to batches. Do not send those to device yet to save space
@@ -117,8 +124,17 @@ validset    = TensorDataset(X_validation,y_validation)
 trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True, drop_last=True, num_workers=4)
 validloader = DataLoader(validset, batch_size=batch_size, shuffle=True, drop_last=True, num_workers=4)
 
+#Set up the model and optimizer
+model = MLP(input_dim=input_size,output_dim=out_size,int_dim=128,N_layer=3).to(device)
+
+optimizer = torch.optim.Adam(model.parameters())
+
+reduce_lr = True#reducing learning rate on plateau
+if reduce_lr==True:
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min',patience=10)
+
 #training
-n_epoch=200#for trial test purpose
+n_epoch=1000#for trial test purpose
 losses_train = []
 losses_vali = []
 
@@ -131,12 +147,16 @@ for n in range(n_epoch):
         X = data[0].to(device)# send to device one by one
         Y_batch = data[1].to(device)# send to device one by one
         Y_pred  = model(X)
+        #print((Y_pred), 'pred')
+
+        Y_pred=torch.reshape(Y_pred, (256,4998, 3))
         diff = (Y_batch - Y_pred)*Y_std# Scale back to unit by *Y_std
+        
 
         
-        loss1 = (diff@covinv)@torch.t(diff)
+        loss1 = torch.einsum('kli,lij,klj->k',diff,covinv,diff)# implement with torch.einsum
         
-        loss=torch.mean(torch.diagonal(loss1,0))# torch.diagonal will only give you the diagonal part of the matrix
+        loss=torch.mean(loss1)
         losses.append(loss.cpu().detach().numpy())
         optimizer.zero_grad()
         loss.backward()
@@ -152,12 +172,13 @@ for n in range(n_epoch):
             X_v       = data[0].to(device)
             Y_v_batch = data[1].to(device)
             Y_v_pred = model(X_v) 
+            Y_v_pred=torch.reshape(Y_v_pred, (256,4998, 3))
             v_diff = (Y_v_batch - Y_v_pred )*Y_std
             
-            loss1 = (v_diff@covinv)@torch.t(v_diff)
+            loss1 = torch.einsum('kli,lij,klj->k',v_diff,covinv,v_diff)
 
             #print(loss)
-            loss_vali=torch.mean(torch.diagonal(loss1,0))
+            loss_vali=torch.mean(loss1)
             losses.append(loss_vali.cpu().detach().numpy())
 
         losses_vali.append(np.mean(losses))
@@ -177,7 +198,7 @@ for n in range(n_epoch):
 
 
 # Save the model and extra parameters
-PATH = "./trainedemu/trialb256"
+PATH = "./trainedemu/trial1milaccb256"
 torch.save(model.state_dict(), PATH+'.pt')
 extrainfo={'X_mean':X_mean,'X_std':X_std,'Y_mean':Y_mean,'Y_std':Y_std}
 np.save(PATH+'.npy',extrainfo)
