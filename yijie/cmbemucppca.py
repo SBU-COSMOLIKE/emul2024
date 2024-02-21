b35dbedac39d299c6609cc7e95f5ece49701a24b
JerreYjz <47430015+JerreYjz@users.noreply.github.com>
2024-02-21 14:33:51 -0500

    Update cmbemucppca.py

--- yijie/cmbemucppca.py
+++ yijie/cmbemucppca.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import numpy as np
 import sys, os
 from torch.utils.data import Dataset, DataLoader, TensorDataset
-from sklearn.decomposition import IncrementalPCA
 
 '''
 N_thread=10
@@ -90,12 +89,11 @@ class MLP(nn.Module):
 #covinv=torch.Tensor(covinv).to(device) #This is inverse of the Covariance Matrix
 
 #load in data
-samples=np.load('YZ_samples/LHS/coslhc_acc.npy',allow_pickle=True)[:5000]# This is actually a latin hypercube sampling of 1mil points
-input_size=len(samples[0])
-data_vectors=np.load('YZ_samples/LHS/coslhc_acc_output.npy',allow_pickle=True)[:5000,:camb_ell_range,0]
-data_vectors=np.log(data_vectors)
-n_pcas=512#testing cosmopower parameter choice
-out_size=n_pcas#1*len(data_vectors[0])
+train_samples=np.load('YZ_samples/LHS/coslhc_acc.npy',allow_pickle=True)# This is actually a latin hypercube sampling of 1mil points
+input_size=len(train_samples[0])
+train_data_vectors=np.load('YZ_samples/LHS/coslhc_acc_pca.npy',allow_pickle=True)
+
+out_size=len(train_data_vectors[0])
 #assign training and validation sets
 model = MLP(input_dim=input_size,output_dim=out_size,int_dim=4,N_layer=4)
 optimizer = torch.optim.Adam(model.parameters())
@@ -106,34 +104,25 @@ if reduce_lr==True:
 model = nn.DataParallel(model)
 model.to(device)
 
-vnum=0
-train_samples=[]
-train_data_vectors=[]
-validation_samples=[]
-validation_data_vectors=[]
-for ind in range(len(samples)):
-    samp=samples[ind]
-    if (0.01<samp[0]<0.035) and (0.005<samp[1]<0.85) and (30<samp[2]<90) and (0.02<samp[3]<0.75) and (0.8<samp[4]<1.2) and (1.7<samp[5]<4.5) and vnum<=int(1e2):
-        validation_samples.append(samp)
-        validation_data_vectors.append(data_vectors[ind])
-        vnum+=1
-    else:
-        train_samples.append(samp)
-        train_data_vectors.append(data_vectors[ind])
+validation_samples=np.load('YZ_samples/Uniform/input/cosuni_acc_10.npy',allow_pickle=True)
 
+validation_data_vectors=np.load('YZ_samples/Uniform/output/cosuni_10_output_pca.npy',allow_pickle=True)
 
-train_samples=np.array(train_samples)
-train_data_vectors=np.array(train_data_vectors)
-validation_samples=np.array(validation_samples)
-validation_data_vectors=np.array(validation_data_vectors)
+extra_info=np.load('YZ_samples/PCAcomp/msett_sampleinfo.npy',allow_pickle=True)
+samp_mean=extra_info.item()['sample_mean']
+samp_std=extra_info.item()['sample_std']
+samp_mean=torch.Tensor(samp_mean).to(device)
+samp_std=torch.Tensor(samp_std).to(device)
 
 train_samples=torch.Tensor(train_samples)
+train_data_vectors=torch.Tensor(train_data_vectors)
 validation_samples=torch.Tensor(validation_samples)
+validation_data_vectors=torch.Tensor(validation_data_vectors)
 
 X_mean=torch.Tensor(train_samples.mean(axis=0, keepdims=True))
 X_std  = torch.Tensor(train_samples.std(axis=0, keepdims=True))
-Y_mean=train_data_vectors.mean(axis=0, keepdims=True)
-Y_std=train_data_vectors.std(axis=0, keepdims=True)
+Y_mean=torch.Tensor(train_data_vectors.mean(axis=0, keepdims=True))
+Y_std=torch.Tensor(train_data_vectors.std(axis=0, keepdims=True))
 X_train=(train_samples-X_mean)/X_std
 X_train[:,6:]=0 # we didn't vary the last 3 parameters: mnu, w, and wa in this test, so setting them to 0 automatically after normalization
 y_train=(train_data_vectors-Y_mean)/Y_std
@@ -141,45 +130,22 @@ y_train=(train_data_vectors-Y_mean)/Y_std
 X_validation=(validation_samples-X_mean)/X_std
 X_validation[:,6:]=0 # we didn't vary the last 3 parameters: mnu, w, and wa in this test, so setting them to 0 automatically after normalization
 y_validation=(validation_data_vectors-Y_mean)/Y_std
-Y_std=torch.tensor(Y_std).to(device)
+Y_std=Y_std.to(device)
 
-
-TTPCA = IncrementalPCA(n_components=n_pcas,batch_size=len(y_train))
-#TEPCA = IncrementalPCA(n_components=n_pcas,batch_size=len(train_samples))
-#EEPCA = IncrementalPCA(n_components=n_pcas,batch_size=len(train_samples))
-
-TTPCA.partial_fit(y_train)
-#TEPCA.partial_fit(train_data_vectors[:,:,1])
-#EEPCA.partial_fit(train_data_vectors[:,:,2])
-transform_matrix=torch.Tensor(TTPCA.components_).to(device)
-#train_pca=np.zeros((len(train_samples), n_pcas, 3), dtype="float32")
-
-train_pca=TTPCA.transform(y_train)
-train_pca=torch.Tensor(train_pca)
-
-#train_pca[:,:,1]=TTPCA.transform(train_data_vectors[:,:,1])
-#train_pca[:,:,2]=TTPCA.transform(train_data_vectors[:,:,2])
-
-#vali_pca=np.zeros((len(validation_samples), n_pcas, 3), dtype="float32")
-
-vali_pca=TTPCA.transform(y_validation)
-vali_pca=torch.Tensor(vali_pca)
-
-#normalizing samples and data vectors to mean 0, std 1
-
-#load the data to batches. Do not send those to device yet to save space
+transform_matrix=np.load('YZ_samples/PCAcomp/msett_comp.npy',allow_pickle=True)
+transform_matrix=torch.Tensor(transform_matrix).to(device)
 
 
 batch_size=512
-trainset    = TensorDataset(X_train, train_pca)
-validset    = TensorDataset(X_validation,vali_pca)
+trainset    = TensorDataset(X_train, y_train)
+validset    = TensorDataset(X_validation,y_validation)
 trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True, drop_last=True, num_workers=1)
 validloader = DataLoader(validset, batch_size=batch_size, shuffle=True, drop_last=True, num_workers=1)
 
 #Set up the model and optimizer
 
 #training
-n_epoch=400#for trial test purpose
+n_epoch=350#for trial test purpose
 losses_train = []
 losses_vali = []
 
@@ -192,15 +158,12 @@ for n in range(n_epoch):
         X = data[0].to(device)# send to device one by one
         Y_batch = data[1].to(device)# send to device one by one
         Y_pred  = model(X).to(device)
-        diff_b=(Y_batch-Y_pred).cpu().detach().numpy()
+        diff_b=(Y_batch-Y_pred)*Y_std
+        diff=torch.matmul(diff_b,transform_matrix)*samp_std
         #Y_pred_unpca=torch.Tensor(TTPCA.inverse_transform(diff_b)).to(device)#print((Y_pred), 'pred')
 
         #Y_pred=torch.reshape(Y_pred, (batch_size,n_pcas,3))
         
-        diff =TTPCA.inverse_transform(diff_b)# Scale back to unit by *Y_std
-        diff=torch.Tensor(diff).to(device)
-        diff=diff*Y_std
-        diff.requires_grad_(True)
         loss1 = torch.sqrt(torch.einsum('kl,kl->k',diff,diff))# implement with torch.einsum
         #print(loss1)
         loss=torch.mean(loss1)
@@ -223,16 +186,8 @@ for n in range(n_epoch):
             X_v       = data[0].to(device)
             Y_v_batch = data[1].to(device)
             Y_v_pred = model(X_v).to(device)
-            diff_v_b=(Y_v_batch-Y_v_pred).cpu().detach().numpy()
-            v_diff =TTPCA.inverse_transform(diff_v_b)# Scale back to unit by *Y_std
-            v_diff=torch.Tensor(v_diff).to(device)
-            v_diff=v_diff*Y_std
-            v_diff.requires_grad_(True)
-
-            #v_diff_b=np.zeros((batch_size, camb_ell_range, 3), dtype="float32")
-            #v_diff_b=np.dot(v_diff, transform_matrix)
-            #v_diff_b[:,:,1]=np.dot(v_diff[:,:,1], TEPCA.components_)
-            #v_diff_b[:,:,2]=np.dot(v_diff[:,:,2], EEPCA.components_)
+            diff_v_b=(Y_v_batch-Y_v_pred)*Y_std
+            v_diff =torch.matmul(diff_v_b,transform_matrix)*samp_std# Scale back to unit by *Y_std
             
             loss1 = torch.sqrt(torch.einsum('kl,kl->k',v_diff,v_diff))
 
@@ -260,12 +215,12 @@ for n in range(n_epoch):
 
 
 # Save the model and extra parameters
-PATH = "./trainedemucp/pca"+str(batch_size)
+PATH = "./trainedemupca/pca"+str(batch_size)
 torch.save(model.state_dict(), PATH+'.pt')
 extrainfo={'X_mean':X_mean,'X_std':X_std,'Y_mean':Y_mean,'Y_std':Y_std}
 np.save(PATH+'.npy',extrainfo)
 np.save(PATH+'losstrain.npy',losses_train)
 np.save(PATH+'lossvali.npy',losses_vali)
-np.save(PATH+'TTpca.npy',TTPCA.components_)
+
 #np.save(PATH+'TEpca.npy',TEPCA.components_)
 #np.save(PATH+'EEpca.npy',EEPCA.components_)
