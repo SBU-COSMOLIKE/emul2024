70aa1aaac27b3313b85097856306dfa69c082c4f
JerreYjz <47430015+JerreYjz@users.noreply.github.com>
2024-10-01 14:16:53 -0400

    Update plot12.py

--- yijie/Plots/Plot12/plot12.py
+++ yijie/Plots/Plot12/plot12.py
@@ -6,18 +6,27 @@ matplotlib.use('TKAgg')# This is for windows, you may not need this for Mac/Linu
 
 
 pertail128=np.array([0.5169,0.1696,0.1055,0.0807 ,0.0607])
+pertail1283b=np.array([0.5168 , 0.1645, 0.0782,0.0680, 0.0567])
 
 pertail32=np.array([0.2617,0.0217,0.0176,0.0166 ,0.0161])
+pertail323b=np.array([0.2974,0.0196,0.0170,0.0164,0.0162])
 
 pertail8=np.array([0.1462,0.0062,0.0043,0.0045,0.0045])
+pertail83b=np.array([0.1949,0.0053,0.0049,0.0048,0.0044])
 
 n_train=np.array([100,200,300,400,530])
 
-plt.plot(n_train,pertail8,label='T=8')
+plt.plot(n_train,pertail8,'b-',label='T=8, 1 Block')
 
-plt.plot(n_train,pertail32,label='T=32')
+plt.plot(n_train,pertail32,'r-',label='T=32, 1 Block')
 
-plt.plot(n_train,pertail128,label='T=128')
+plt.plot(n_train,pertail128,'g-',label='T=128, 1 Block')
+
+plt.plot(n_train,pertail83b,'b--',label='T=8, 3 Block')
+
+plt.plot(n_train,pertail323b,'r--',label='T=32, 3 Block')
+
+plt.plot(n_train,pertail1283b,'g--',label='T=128, 3 Block')
 
 plt.xlabel('Number of training points(thousands)')
 plt.ylabel(r'Fraction of points with $\chi^2>0.2$')
