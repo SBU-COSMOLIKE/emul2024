1b7942cb7d99c01f5c048b1391d5ac635b9a1e5d
JerreYjz <47430015+JerreYjz@users.noreply.github.com>
2024-10-07 01:31:22 -0400

    new plot, switched slides

---
+++ yijie/Plots/Plot16/plot16.py
@@ -0,0 +1,44 @@
+
+import numpy as np
+import matplotlib
+from matplotlib import pyplot as plt
+
+# GENERAL PLOT OPTIONS
+matplotlib.rcParams['mathtext.fontset'] = 'stix'
+matplotlib.rcParams['font.family'] = 'STIXGeneral'
+matplotlib.rcParams['mathtext.rm'] = 'Bitstream Vera Sans'
+matplotlib.rcParams['mathtext.it'] = 'Bitstream Vera Sans:italic'
+matplotlib.rcParams['mathtext.bf'] = 'Bitstream Vera Sans:bold'
+matplotlib.rcParams['xtick.bottom'] = True
+matplotlib.rcParams['xtick.top'] = False
+matplotlib.rcParams['ytick.right'] = False
+matplotlib.rcParams['axes.edgecolor'] = 'black'
+matplotlib.rcParams['axes.linewidth'] = '1.0'
+matplotlib.rcParams['axes.labelsize'] = 'medium'
+matplotlib.rcParams['axes.grid'] = True
+matplotlib.rcParams['grid.linewidth'] = '0.0'
+matplotlib.rcParams['grid.alpha'] = '0.18'
+matplotlib.rcParams['grid.color'] = 'lightgray'
+matplotlib.rcParams['legend.labelspacing'] = 0.77
+matplotlib.rcParams['savefig.bbox'] = 'tight'
+matplotlib.rcParams['savefig.format'] = 'pdf'
+
+
+matplotlib.use('TKAgg')# This is for windows, you may not need this for Mac/Linux
+
+plt.figure(figsize = (5,5))
+
+cambtrue=np.load('cambtrue.npy', allow_pickle=True)
+cambemul=np.load('cambemul.npy', allow_pickle=True)
+
+ell=np.arange(2,5000,1)
+plt.plot(cambemul*ell*(ell+1),'.',label='emulator')
+plt.plot(cambtrue*ell*(ell+1),label='camb')
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel(r'$\ell$')
+plt.ylabel(r'$C_{\ell}^{TT}$')
+plt.legend()
+
+plt.savefig("plot16.pdf", format="pdf", bbox_inches="tight", dpi=300, pad_inches=0.05)
+plt.savefig("plot16.svg", format="svg", bbox_inches="tight", dpi=300, pad_inches=0.05)
